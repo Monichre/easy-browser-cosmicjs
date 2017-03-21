87f372975ebb100845c29dd5a565ae7ef085c9d2
Liam Ellis <liamhellis@gmail.com>
2017-03-21 14:07:13 -0400

    sample blog post working using my own data/bucket

--- app.browser.js
+++ app.browser.js
@@ -1,14 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Cosmic = require('cosmicjs');
 
-const bucket = { slug: 'easy-browser-example' }
-const object = { slug: 'home' }
+const bucket = { slug: 'blog-attempt' }
+const object = { slug: 'sample-blog-post' }
 
 Cosmic.getObject({ bucket}, object, (err, res) => {
     var object = res.object
     document.getElementById('title').innerHTML = object.title
     document.getElementById('content').innerHTML = object.content
-    document.getElementById('metafields').innerHTML = '<pre>' + JSON.stringify(object.metafields, null, 2) + '</pre>'
+
 })
 
 },{"cosmicjs":2}],2:[function(require,module,exports){
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var Cosmic = require('cosmicjs');
 
-const bucket = { slug: 'easy-browser-example' }
-const object = { slug: 'home' }
+const bucket = { slug: 'blog-attempt' }
+const object = { slug: 'sample-blog-post' }
 
 Cosmic.getObject({ bucket}, object, (err, res) => {
     var object = res.object
     document.getElementById('title').innerHTML = object.title
     document.getElementById('content').innerHTML = object.content
-    document.getElementById('metafields').innerHTML = '<pre>' + JSON.stringify(object.metafields, null, 2) + '</pre>'
+
 })
